48551de9ba006485b712169e95e8f24faaa8fd37
Hedley Smith <hedley@agilecollective.com>
2013-01-06 18:15:30 +0000

    Adding some new changes

--- includes/global/page_footer.php
+++ includes/global/page_footer.php
@@ -1,21 +1,16 @@
 	<footer class="clearfix main-container" role="contentinfo">
 	  <div class="footer-inner container-full">
 	    <div class="region region-footer">
-	      <section id="block-block-2" class="block block-block grid_5">
-	        <div class="block-inner">
-	          <h2>Connect with us</h2>
-
-	          <div class="content">
-Twitter | Google+ | Facebook
-	          </div>
-	        </div>
-	      </section>
-	      <section id="block-block-11" class="block block-block grid_3">
+	      <section id="block-social-media" class="block block-block grid_5">
 	        <div class="block-inner">
 
 	          <div class="content">
-	            <p>Footer notes</p>
-	            <div> </div>
+	          <ul>
+	          	<li><img src="/img/social_twitter.png" /></li>
+							<li><img src="/img/social_facebook.png" /></li>
+							<li><img src="/img/social_gplus.png" /></li>
+							<li><img src="/img/social_email.png" /></li>
+						</ul>
 	          </div>
 	        </div>
 	      </section>
--- layouts/homepage1.php
+++ layouts/homepage1.php
@@ -8,7 +8,8 @@
 <section id="highlight">
   <div>
     <div class="slide">
-      We are pleased to invite everyone to the <strong>UK’s capital</strong> to join the <strong>brightest minds in Drupal</strong> for a <strong>three day Drupal Camp</strong> which will bring together Drupal developers, themers, users and those interested in learning more about Drupal.
+      <h1>DrupalCamp London 2013</h1>
+      <p>We are pleased to invite everyone to the <strong>UK’s capital</strong> to join the <strong>brightest minds in Drupal</strong> for a <strong>three day Drupal Camp</strong> which will bring together Drupal developers, themers, users and those interested in learning more about Drupal.</p>
     </div>
 
   </div>
@@ -20,70 +21,129 @@
   <img src="/img/add-a-session.png" />
   <img src="/img/buy-tickets.png" />
   <img src="/img/business-day.png" />
+  </div>
+</section>
+
+<section id="featured-speakers">
+  <div class="container-full">
     <ul class="grid">
-      <li>
-        <div>
-          Speakers
+      <li class="wide">
+        <div class="featured-speaker" id="josh">
+          <h3>Josh Koenig</h3>
+          <p class="squeeze-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </li>
       <li class="wide">
-        <div>
-          How to get here
+        <div class="featured-speaker" id="addison">
+          <h3>Addison Berry</h3>
+          <p class="squeeze-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </li>
-      <li>
-        <div>
-          Tickets / Schedule
-        </div>
-      </li>     
-      <li>
-        <div>
-          What to expect
-        </div>
-      </li>
-      <li>
-        <div>
-          The Drupal Community
-        </div>
-      </li>
-      <li class="wide">
-        <div>
-          Sponsors
-        </div>
-      </li>
-      <li>
-        <div>
-          What is Drupal?
-        </div>
-      </li>
-      <li class="wide">
-        <div>
-          What's new in Drupal?
-          <small>Drupal 7 / 8...</small>
-        </div>
-      </li>
-      <li>
-        <div>
-          [Social Media Icons]
-        </div>
-      </li>
-      <li class="wide">
-        <div>
-          Inspirational Drupal stories
-        </div>
-      </li>
-      <li>
-        <div>
-          <a href="#">5</a>
-        </div>
-      </li>
-      <li>
-        <div>
-          <a href="#">6</a>
+      <li class="full-width">
+        <div class="all-speakers">
+          <ul id="featured-speakers-2">
+            <li><img src="/img/placeholder_person_2.png" /><div><strong>Joe Bloggs</strong><p>Lorem ipsum dolor sit amet</p></div></li>
+            <li><img src="/img/placeholder_person_3.jpg" /><div><strong>Peter Piper</strong><p>Lorem ipsum dolor</p></div></li>
+            <li><img src="/img/placeholder_person_4.jpeg" /><div><strong>Joe Bloggs</strong><p>Lorem ipsum dolor sit amet</p></div></li>
+            <li><img src="/img/placeholder_person_5.jpg" /><div><strong>Peter Piper</strong><p>Lorem ipsum dolor sit</p></div></li>            
+          </ul>
+          <ul id="speakers-list">
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+            <li>Joe Bloggs</li>
+          </ul>
         </div>
       </li>
     </ul>
-    </div>
+  </div>
+</section>
+
+<section id="drupal-info">
+  <div class="container-full">
+    <ul class="grid">
+      <li class="full-width">
+        <h1>Schedule</h1>
+        <div class="all-speakers">
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+
+<section id="whos-going">
+  <div class="container-full">
+    <ul class="grid">
+      <li class="full-width">
+        <h1>Who's going?</h1>
+        <div class="all-speakers">
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+
+<section id="drupal-info">
+  <div class="container-full">
+    <ul class="grid">
+      <li class="full-width">
+        <h1>What is Drupal?</h1>
+        <div class="all-speakers">
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+
+<section id="drupal-info">
+  <div class="container-full">
+    <ul class="grid">
+      <li class="full-width">
+        <h1>The London Drupal Community</h1>
+        <div class="all-speakers">
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+
+
+  <section id="sponsors">
+    <h2>Sponsors</h2>
+    <img src="/img/sponsor_capgemini.jpg"/>
+    <img src="/img/sponsor_acquia.jpg" />
+    <img src="/img/sponsor_brightlemon.jpg" />
+    <img src="/img/sponsor_cameron-wilding.png" />
   </section>
+
+  <section id="strapline">
+    <h1>Meet, Learn, Play.</h1>
+  </section>
+
 
 <?php include('../includes/global/page_footer.php'); ?>
